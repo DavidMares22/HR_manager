5abf333b6c6688f3bb67b15d68d3977f83d0573f
damr <damr18@gmail.com>
2021-07-05 10:00:42 -0500

    added some endpoints PUT,DELETE,POST

--- HR_manager/Server/Controllers/DepartmentController.cs
+++ HR_manager/Server/Controllers/DepartmentController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using HR_manager.Server.Data;
 using HR_manager.Server.IRepository;
 using HR_manager.Server.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -42,7 +44,7 @@ namespace HR_manager.Server.Controllers
             }
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetDepartment")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetDepartment(int id)
@@ -58,5 +60,64 @@ namespace HR_manager.Server.Controllers
                 return StatusCode(500, "Internal Server Error. Please Try Again Later.");
             }
         }
+
+        [Authorize]
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateDepartment(int id, [FromBody] CreateDepartmentDTO departmentDTO)
+        {
+            if (!ModelState.IsValid || id < 1)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var department = await _unitOfWork.Departments.Get(q => q.Id == id);
+                if (department == null)
+                {
+                    return BadRequest("Submitted data is invalid");
+                }
+
+                _mapper.Map(departmentDTO, department);
+                _unitOfWork.Departments.Update(department);
+                await _unitOfWork.Save();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Please Try Again Later.");
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateDepartment([FromBody] CreateDepartmentDTO departmentDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var department = _mapper.Map<Department>(departmentDTO);
+                await _unitOfWork.Departments.Insert(department);
+                await _unitOfWork.Save();
+
+                return CreatedAtRoute("GetDepartment", new { id = department.Id }, department);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Please Try Again Later.");
+            }
+        }
+
     }
 }
--- HR_manager/Server/Controllers/EmployeeController.cs
+++ HR_manager/Server/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using HR_manager.Server.Data;
 using HR_manager.Server.IRepository;
 using HR_manager.Server.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -41,8 +42,7 @@ namespace HR_manager.Server.Controllers
             }
         }
         
-        [Authorize]
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetEmployee")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetEmployee(int id)
@@ -52,6 +52,99 @@ namespace HR_manager.Server.Controllers
                 var employee = await _unitOfWork.Employees.Get(q => q.Id == id, new List<string> { "Department" });
                 var result = _mapper.Map<EmployeeDTO>(employee);
                 return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Please Try Again Later.");
+            }
+        }
+
+
+        [Authorize]
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateEmployee(int id, [FromBody] CreateEmployeeDTO employeeDTO)
+        {
+            if (!ModelState.IsValid || id < 1)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var employee = await _unitOfWork.Employees.Get(q => q.Id == id);
+                if (employee == null)
+                {
+                    return BadRequest("Submitted data is invalid");
+                }
+
+                _mapper.Map(employeeDTO, employee);
+                _unitOfWork.Employees.Update(employee);
+                await _unitOfWork.Save();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Please Try Again Later.");
+            }
+        }
+
+
+
+        [Authorize]
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var employees = await _unitOfWork.Employees.Get(q => q.Id == id);
+                if (employees == null)
+                {
+                    return BadRequest("Submitted data is invalid");
+                }
+
+                await _unitOfWork.Employees.Delete(id);
+                await _unitOfWork.Save();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Please Try Again Later.");
+            }
+        }
+
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateEmployee([FromBody] CreateEmployeeDTO employeeDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);  
+            }
+
+            try
+            {
+                var employee = _mapper.Map<Employee>(employeeDTO);
+                await _unitOfWork.Employees.Insert(employee);
+                await _unitOfWork.Save();
+
+                return CreatedAtRoute("GetEmployee", new { id = employee.Id }, employee);
             }
             catch (Exception ex)
             {
