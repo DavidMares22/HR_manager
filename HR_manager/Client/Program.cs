2a031de561172bbfa38df63325a3bbe9b507b10e
damr <damr18@gmail.com>
2021-07-05 18:06:41 -0500

    Basic auth and aut for the client app

--- HR_manager/Client/Program.cs
+++ HR_manager/Client/Program.cs
@@ -1,3 +1,5 @@
+using HR_manager.Client.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +20,9 @@ namespace HR_manager.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, DummyAuthenticationStateProvider>();
 
             await builder.Build().RunAsync();
         }
